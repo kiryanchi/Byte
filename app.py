76e88a2deecdf0b76c1b66c616b4ed169a243cd0
gk7734 <127005459+gk7734@users.noreply.github.com>
2023-06-23 06:41:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,24 @@
 import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def index():
    print('Request for index page received')
    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+@app.route('/getJson', methods=['POST'])
+def get_json():
+    data = request.json['inputData']
+    response = requests.get(f"https://www.daegufood.go.kr/kor/api/tasty.html?mode=json&addr={data}")
+    info = response.json()
+    return jsonify(info)
 
 if __name__ == '__main__':
    app.run()
